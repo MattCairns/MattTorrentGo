4f523a73d9bdf7c2b84108d685b86195fa7866f0
Matthew Cairns <mattrcairns@gmail.com>
2018-10-13 15:00:15 -0700

    Read first bytes

--- decoder.go
+++ decoder.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readDictionary(r *bufio.Reader) {
+	b, err := r.ReadByte()
+	check(err)
+
+	fmt.Println(string(b))
+}
+
+func readInteger() {
+
+}
+
+func readBytes() {
+
+}
+
+func readList() {
+
+}
 
 func main() {
-	fmt.Println("vim-go")
+	f, err := os.Open("debian.torrent")
+	check(err)
+
+	read := bufio.NewReader(f)
+
+	b, err := read.ReadByte()
+	check(err)
+	if string(b) == "d" {
+		fmt.Println("Dict")
+		d_size, err := read.ReadByte()
+		check(err)
+		fmt.Printf("Dict size %s", string(d_size))
+	}
 }
